6586f222eddfd8ee1bf7e5c106cb868540b2bd2a
user <user@SAIPATEL-PC>
2023-03-21 17:43:37 +0530

    Demo Test

---
+++ JDBC Dao Layers/src/com/ciq/BatchTransactions/DemoTesting.java
@@ -0,0 +1,11 @@
+package com.ciq.BatchTransactions;
+
+public class DemoTesting {
+	public static void main(String[] args) {
+		
+		
+		String s = "Checking for GitHub update";
+		System.out.println(s);
+	}
+
+}
